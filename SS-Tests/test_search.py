3dbf6fd1a898782b5b4565a2d067c84d557c265d
Romaldik <romasepel955@gmail.com>
2026-04-07 16:49:50 +0300

    new_autoTest

---
+++ SS-Tests/test_search.py
@@ -0,0 +1,37 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+BASE_URL = "https://www.greencity.cx.ua/#/greenCity/events"
+
+if __name__ == "__main__":
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=options)
+    
+    driver.maximize_window()
+    driver.get(BASE_URL)
+   
+    sleep(5) 
+    
+    search_icon_xpath = "//div[contains(@class, 'container-img')]"
+    search_icon = driver.find_element(By.XPATH, search_icon_xpath)
+    search_icon.click()
+
+    sleep(2)
+    
+    search_input_xpath = "//input[@placeholder='Search']"
+    search_input = driver.find_element(By.XPATH, search_input_xpath)
+    
+    search_text = "Environment"
+    search_input.send_keys(search_text + "\n")
+    sleep(5)
+
+    try:
+        result_card = driver.find_element(By.XPATH, "//div[(@class='image-container')]")
+        if result_card.is_displayed():
+            print("Є івенти!")
+    except:
+        print("Немає івентів!")
+
+    driver.quit()
